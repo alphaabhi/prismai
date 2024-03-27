c7a974400ca93b4a0e06372721a50c071c311c36
unknown <ajha929@gmail.com>
2024-03-28 00:30:21 +0530

    initial commit

---
+++ middleware.ts
@@ -0,0 +1,17 @@
+import { authMiddleware } from "@clerk/nextjs";
+ 
+
+ 
+export default authMiddleware({
+
+});
+ 
+export const config = {
+  matcher: [
+  
+ 
+    "/((?!.+\\.[\\w]+$|_next).*)",
+
+    "/(api|trpc)(.*)"
+  ]
+};
