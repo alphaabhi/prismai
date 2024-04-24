9f4b18f3406e634b8b653c5d4e82fa5017f01691
unknown <ajha929@gmail.com>
2024-04-25 01:22:23 +0530

    minor change

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -14,7 +14,8 @@ const Home = async ({ searchParams }: SearchParamProps) => {
     <>
       <section className="home">
         <h1 className="home-heading">
-        Dive into a world where your imagination knows no bounds
+        STOP WORKING SO HARD 
+        BE SMART WITH PRISMAI
         </h1>
         <ul className="flex-center w-full gap-20">
           {navLinks.slice(1, 5).map((link) => (
