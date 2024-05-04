1b255bf1929d10fc9ad8a33f9d8f7c21a376e173
unknown <ajha929@gmail.com>
2024-05-05 00:12:08 +0530

    home page banner update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -14,16 +14,14 @@ const Home = async ({ searchParams }: SearchParamProps) => {
     <>
       <section className="home">
         <h1 className="home-heading">
-        STOP WORKING HARD 
-        BE SMART WITH
-         PRISMAI
+       Amazingly easy content creation?
         </h1>
-        <ul className="flex-center w-full gap-20">
+        <ul className="flex-center w-full gap-30">
           {navLinks.slice(1, 5).map((link) => (
             <Link
               key={link.route}
               href={link.route}
-              className="flex-center flex-col gap-2"
+              className="flex-center flex-col agap-2"
             >
               <li className="flex-center w-fit rounded-full bg-white p-4">
                 <Image src={link.icon} alt="image" width={24} height={24} />
