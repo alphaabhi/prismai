b2bf5966bd4103fa42f6c8f08f00657c4a33b359
unknown <ajha929@gmail.com>
2024-05-06 00:19:28 +0530

    text-bold update in css

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -12,37 +12,38 @@ const Home = async ({ searchParams }: SearchParamProps) => {
   const images = await getAllImages({ page, searchQuery})
 
   return (
-    <div className="page-wrapper">  {/* Wrapper to ensure footer is at the bottom of the page */}
-      <section className="home">
-        <h1 className="home-heading">
-          Amazingly easy content creation.
-        </h1>
-        <ul className="flex-center w-full gap-30">
-          {navLinks.slice(1, 5).map((link) => (
-            <Link
-              key={link.route}
-              href={link.route}
-              className="flex-center flex-col agap-2"
-            >
-              <li className="flex-center w-fit rounded-full bg-white p-4">
-                <Image src={link.icon} alt="image" width={24} height={24} />
-              </li>
-              <p className="p-14-medium text-center text-white">{link.label}</p>
-            </Link>
-          ))}
-        </ul>
-      </section>
+    <div className="flex flex-col min-h-screen"> {/* Ensures footer can stick to the bottom */}
+      <main className="flex-grow">
+        <section className="home">
+          <h1 className="home-heading">
+            Amazingly easy content creation.
+          </h1>
+          <ul className="flex-center w-full gap-30">
+            {navLinks.slice(1, 5).map((link) => (
+              <Link
+                key={link.route}
+                href={link.route}
+                className="flex-center flex-col gap-2"
+              >
+                <li className="flex-center w-fit rounded-full bg-white p-4">
+                  <Image src={link.icon} alt="image" width={24} height={24} />
+                </li>
+                <p className="p-14-medium text-center text-white">{link.label}</p>
+              </Link>
+            ))}
+          </ul>
+        </section>
 
-      <section className="sm:mt-12">
-        <Collection 
-          hasSearch={true}
-          images={images?.data}
-          totalPages={images?.totalPage}
-          page={page}
-        />
-      </section>
-
-      <Footer />  {/* Footer placed outside the main content but inside the page wrapper */}
+        <section className="sm:mt-12">
+          <Collection 
+            hasSearch={true}
+            images={images?.data}
+            totalPages={images?.totalPage}
+            page={page}
+          />
+        </section>
+      </main>
+      <Footer /> {/* Footer is outside the main content to ensure it sticks at the bottom */}
     </div>
   )
 }
