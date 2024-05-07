74c2c581652ea3ad08836a4cb083c96462457f43
unknown <ajha929@gmail.com>
2024-05-07 15:56:06 +0530

    mobile nav logo update

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -39,7 +39,7 @@ const MobileNav = () => {
             <SheetContent className="sheet-content sm:w-64">
               <>
                 <Image 
-                  src="/assets/images/logo-text.svg"
+                  src="/assets/images/prismlogo-text.svg"
                   alt="logo"
                   width={152}
                   height={23}
